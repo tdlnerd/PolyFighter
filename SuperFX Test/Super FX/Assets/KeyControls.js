ee5fba4e67e82c583d054bbbaef2437975ef9725
Luce Tanner <tanner.luce17@beltonnewtech.org>
2016-03-23 10:44:12 -0500

    Control Fixes

--- SuperFX Test/Super FX/Assets/KeyControls.js
+++ SuperFX Test/Super FX/Assets/KeyControls.js
@@ -6,16 +6,16 @@ function Start () {
 
 function Update () {
 	if (Input.GetKey("left")) {
-		transform.Rotate(Vector2.down * (Time.deltaTime * rotatespeed));
+		transform.Translate(Vector2.left * (Time.deltaTime * rotatespeed));
 		}
 	if (Input.GetKey("right")) {
-	transform.Rotate(Vector2.up * (Time.deltaTime * rotatespeed));
+	transform.Translate(Vector2.right * (Time.deltaTime * rotatespeed));
 	}
 	if (Input.GetKey("up")) {
-	transform.Rotate(Vector2.left * (Time.deltaTime * rotatespeed));
+	transform.Translate(Vector2.up * (Time.deltaTime * rotatespeed));
 	}
 	if (Input.GetKey("down")) {
-	transform.Rotate(Vector2.right * (Time.deltaTime * rotatespeed));
+	transform.Translate(Vector2.down * (Time.deltaTime * rotatespeed));
 	}
 	if (Input.GetKey("x") && movespeed < 60) {
 	movespeed += 5; 
@@ -23,7 +23,5 @@ function Update () {
 	if (Input.GetKey("z") && movespeed > 10) {
 	movespeed -= 5; 
 	}
-	if (Input.GetKey("space")) {
-	transform.Translate(Vector3.forward * (Time.deltaTime * movespeed));
-	}
+
 }
